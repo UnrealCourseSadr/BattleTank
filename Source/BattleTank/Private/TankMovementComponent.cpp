c480856f95922ce1f22e344f1a2b4d2ba231167b
DESKTOP-DOVB5QM\Mehdi <s.mehdiprog93@gmail.com>
2019-05-19 14:33:22 +0430

    Make tank movement component to take tank forward and backward

--- Source/BattleTank/Private/TankMovementComponent.cpp
+++ Source/BattleTank/Private/TankMovementComponent.cpp
@@ -2,8 +2,19 @@
 
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
+
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+	// TODO Prevent double- speed due to dual control
 }
--- Source/BattleTank/Public/TankMovementComponent.h
+++ Source/BattleTank/Public/TankMovementComponent.h
@@ -6,16 +6,26 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+class UTankTrack;
+
 /**
- * 
+ * Responsible for driving the tank tracks
  */
-UCLASS()
+UCLASS( meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 {
 	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintCallable)
+	UFUNCTION(BlueprintCallable, Category = "Setup")
+	void Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
+
+	// Handle tank movement to forward and backward based on the throw (-1 max backward, and +1 max forward)
+	UFUNCTION(BlueprintCallable, Category = "Movement")
 	void IntendMoveForward(float Throw);
+
+private:
+	UTankTrack* LeftTrack{ nullptr };
+	UTankTrack* RightTrack{ nullptr };
 	
 };
